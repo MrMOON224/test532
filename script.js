8e958d68cf5b33affaad7c54879d196c7060e092
MrMOON224 <90089357+MrMOON224@users.noreply.github.com>
2025-06-29 22:07:16 +0630

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('#menu-icon').on('click', function(){
+    $('.navbar').toggleClass('expand');
+    return false;
+  });
+});
+function myFunction() {
+   var element = document.body;
+   element.classList.toggle("dark-mode");
+}
